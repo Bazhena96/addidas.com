58c4604cb2fa68767b0ec58a3b118a0532d93b6b
Bazhena96 <bazhenakak@gmail.com>
2020-09-06 01:57:12 +0300

    open link added.

--- TestSuite/Utils.py
+++ TestSuite/Utils.py
@@ -8,4 +8,10 @@ def input_text(css_selector, driver, text):
     element.send_keys(text)
 
 
+def open_link(css_selector, driver, keys):
+    link = driver.find_element_by_css_selector(css_selector)
+    link.send_keys(keys)
+
+
+
 
