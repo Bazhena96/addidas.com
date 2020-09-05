06f99767a188dd581f93de9fa1fb68e30992fbad
Bazhena96 <bazhenakak@gmail.com>
2020-09-06 01:47:33 +0300

    wait and click added.

--- TestSuite/TestsCases.py
+++ TestSuite/TestsCases.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+from TestSuite.Utils import find_and_click
+
 
 class AddToCart(unittest.TestCase):
     def setUp(self):
@@ -21,17 +23,37 @@ class AddToCart(unittest.TestCase):
         product_link = self.driver.find_element_by_css_selector("div.grid-item___3rAkS:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)")
         product_link.send_keys(Keys.ENTER)
         # close alert
-        close_alert = self.driver.find_element_by_css_selector(".gl-modal__close > svg:nth-child(1)")
-        close_alert.click()
+        find_and_click(".gl-modal__close > svg:nth-child(1)", self.driver)
         # choose size
-        choose_size = self.driver.find_element_by_css_selector("button.size___TqqSo:nth-child(1)")
-        choose_size.click()
+        find_and_click("button.size___TqqSo:nth-child(1)", self.driver)
         # click add to card
-        add_to_cart = self.driver.find_element_by_css_selector(".add-to-bag___3wgQk > button:nth-child(1)")
-        add_to_cart.click()
+        find_and_click(".add-to-bag___3wgQk > button:nth-child(1)", self.driver)
         # check the product cart and verify that product are added
-        check_cart = self.driver.find_element_by_css_selector("a.gl-cta:nth-child(7)")
-        check_cart.click()
+        find_and_click("a.gl-cta:nth-child(7)", self.driver)
+
+    def test_set_remainder(self):
+        # click the release date button
+        find_and_click("div.glass-navigation-flyout-white___20EjX:nth-child(8) > a:nth-child(1)",self.driver)
+        # close alert
+        find_and_click(".gl-modal__close > svg:nth-child(1)", self.driver)
+        # choose the product to set the remainder
+        find_and_click("div.plc-product-card___1kN_6:nth-child(2) > div:nth-child(1) > a:nth-child(1)",self.driver)
+        # set a remainder
+        find_and_click(".ctaButtonContainer___3a_3R > button:nth-child(1)",self.driver)
+        # add to calendar and choose the date
+        find_and_click(".gl-cta--secondary",self.driver)
+        find_and_click("a.gl-cta--secondary:nth-child(1)", self.driver)
+
+    def test_check_order(self):
+        # click "check the order"
+        find_and_click(".inner___1T3DW > a:nth-child(6)", self.driver)
+        input_number = self.driver.find_element_by_css_selector(".field__text--is-invalid___2523M")
+        input_number.send_keys("200400H")
+        input_email = self.driver.find_element_by_css_selector("div.field___22seD:nth-child(3) > input:nth-child(1)")
+        input_email.send_keys("bazhenkak@gmail.com")
+        # find order
+        find_and_click("button.gl-cta:nth-child(4)", self.driver)
+
 
     def tearDown(self):
         self.driver.quit()
