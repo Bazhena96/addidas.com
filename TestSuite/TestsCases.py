8a6b556a838d52c95fecb72afcc9480ad16197f0
Bazhena96 <bazhenakak@gmail.com>
2020-09-05 23:07:00 +0300

    Test "Add To Cart" added.

--- TestSuite/TestsCases.py
+++ TestSuite/TestsCases.py
@@ -6,24 +6,19 @@ import unittest
 class AddToCart(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(10)  # seconds
         self.driver.maximize_window()
+        self.driver.implicitly_wait(10)  # seconds
         # navigate to the home page
         self.driver.get("https://www.adidas.com")
 
-    def test_add_product_to_cart(self):
-        self.driver = webdriver.Firefox()
+    def test_add_product(self):
         # add text into search box
-        input_field = self.driver.find_element_by_css_selector(".searchinput-wrapper___18TsX > form:nth-child(2)")
+        input_field = self.driver.find_element_by_css_selector('.searchinput___zXLAR')
         input_field.send_keys("short")
         input_field.send_keys(Keys.ENTER)
         titles = self.driver.find_elements_by_class_name("gl-product-card__details-main")
-        for title in titles:
-            assert "short" in title.text()
         # open product link
-        product_link = self.driver.find_element_by_css_selector("div.grid-item___3rAkS:nth-child(1) > div:nth-child("
-                                                                "1) > div:nth-child(1) > div:nth-child(1) > "
-                                                                "div:nth-child(1)")
+        product_link = self.driver.find_element_by_css_selector("div.grid-item___3rAkS:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)")
         product_link.send_keys(Keys.ENTER)
         # close alert
         close_alert = self.driver.find_element_by_css_selector(".gl-modal__close > svg:nth-child(1)")
@@ -36,11 +31,9 @@ class AddToCart(unittest.TestCase):
         add_to_cart.click()
         # check the product cart and verify that product are added
         check_cart = self.driver.find_element_by_css_selector("a.gl-cta:nth-child(7)")
-        check_cart.submit()
         check_cart.click()
 
     def tearDown(self):
-        self.driver = webdriver.Firefox()
         self.driver.quit()
 
 
