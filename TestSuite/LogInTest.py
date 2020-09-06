0d360e3e93a5930c1c957f065a2473b6e43a85ac
Bazhena96 <bazhenakak@gmail.com>
2020-09-06 22:39:07 +0300

    Log In test added.

--- TestSuite/LogInTest.py
+++ TestSuite/LogInTest.py
@@ -4,6 +4,7 @@ import unittest
 
 from TestSuite.Utils import find_and_click, input_and_click, input_text
 
+
 class LogIn(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
@@ -12,5 +13,29 @@ class LogIn(unittest.TestCase):
         # navigate to the home page
         self.driver.get("https://www.adidas.com")
 
-    def
+    def test_log_in(self):
+        find_and_click(self.driver, ".inner___1T3DW > a:nth-child(12)")
+        input_text(self.driver, "#login-email", "bazhenkak@gmail.com")
+        input_text(self.driver, "#login-password", "change1313")
+        find_and_click(self.driver, "div.gl-vspace-bpall-small:nth-child(6) > button:nth-child(1)")
+
+    def test_forgot_password(self):
+        find_and_click(self.driver, ".inner___1T3DW > a:nth-child(12)")
+        find_and_click(self.driver, "div.col-l-10:nth-child(1) > form:nth-child(2) > a:nth-child(1)")
+        input_text(self.driver, ".field__input___3eT4b")
+        find_and_click(self.driver, "div.gl-vspace-bpall-medium:nth-child(1) > button:nth-child(1)")
+
+    def test_join_club(self):
+        find_and_click(self.driver, ".inner___1T3DW > div:nth-child(8) > em:nth-child(1) > a:nth-child(1)")
+        input_text(self.driver, "#registration-email-field", "bazhenkak@gmail.com")
+        input_text(self.driver, "#registration-password-field", "change1313")
+        find_and_click(self.driver, ".gl-form-item--inline")
+        find_and_click(self.driver, "button.gl-cta:nth-child(8)")
+
+
+
+
+
+
+
 
