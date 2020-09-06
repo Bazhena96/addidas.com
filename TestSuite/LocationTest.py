d331786089cf5de0057be9177a10751591b3840f
Bazhena96 <bazhenakak@gmail.com>
2020-09-06 23:42:44 +0300

    "input_and_click" deleted.

--- TestSuite/LocationTest.py
+++ TestSuite/LocationTest.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-from TestSuite.Utils import find_and_click, input_and_click, input_text
+from TestSuite.Utils import find_and_click
 
 
 class ChangeLocation(unittest.TestCase):
