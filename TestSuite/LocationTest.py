aaa98954d4ae7dcca3e6e040461db374ee478240
Bazhena96 <bazhenakak@gmail.com>
2020-09-06 23:38:13 +0300

    Changed location to Poland.

--- TestSuite/LocationTest.py
+++ TestSuite/LocationTest.py
@@ -18,7 +18,8 @@ class ChangeLocation(unittest.TestCase):
         find_and_click(self.driver, ".country_selector_text___yovMX")
         find_and_click(self.driver, "#europe")
         # change location to Poland
-        find_and_click(self.driver, "div.country-group:nth-child(4) > ul:nth-child(3) > li:nth-child(3) > a:nth-child(1) > img:nth-child(1)")
+        find_and_click(self.driver, "div.country-group:nth-child(4) > ul:nth-child(3) > li:nth-child(3) > a:nth-child(1)")
+        assert("Stworzone z myślą o przyrodzie.") in self.driver.page_source
 
     def tearDown(self):
         self.driver.quit()
