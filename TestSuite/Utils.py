3dee77dbd6268b0f64c1df7ee1d1519d08d9c8fb
Bazhena96 <bazhenakak@gmail.com>
2020-09-06 21:40:01 +0300

    new def added.

--- TestSuite/Utils.py
+++ TestSuite/Utils.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.keys import Keys
+
+
 def find_and_click(css_selector, driver):
     element = driver.find_element_by_css_selector(css_selector)
     element.click()
@@ -8,9 +11,10 @@ def input_text(css_selector, driver, text):
     element.send_keys(text)
 
 
-def open_link(css_selector, driver, keys):
-    link = driver.find_element_by_css_selector(css_selector)
-    link.send_keys(keys)
+def input_and_click(css_selector, driver, text):
+    element = driver.find_element_by_css_selector(css_selector)
+    element.send_keys(text)
+    element.send_keys(Keys.ENTER)
 
 
 
