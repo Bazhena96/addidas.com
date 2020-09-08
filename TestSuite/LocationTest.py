003dda1b2074eb9548e3ba162c5d8d8bc1fe0575
Bazhena96 <bazhenakak@gmail.com>
2020-09-09 00:10:31 +0300

    Check that location are changed.

--- TestSuite/LocationTest.py
+++ TestSuite/LocationTest.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+from selenium.common.exceptions import NoSuchElementException
+
 
 from TestSuite.Utils import find_and_click
 
@@ -19,7 +21,12 @@ class ChangeLocation(unittest.TestCase):
         find_and_click(self.driver, "#europe")
         # change location to Poland
         find_and_click(self.driver, "div.country-group:nth-child(4) > ul:nth-child(3) > li:nth-child(3) > a:nth-child(1)")
-        assert("Stworzone z myślą o przyrodzie.") in self.driver.page_source
+        try:
+            text = self.driver.find_element_by_id("POBIERZ, ZAREJESTRUJ SIĘ, WEŹ UDZIAŁ")
+            return True
+        except NoSuchElementException:
+            print('Zero element')
+            return False
 
     def tearDown(self):
         self.driver.quit()
