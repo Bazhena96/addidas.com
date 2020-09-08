1eba7382e1cb57d7e764cb160143645460bc3303
Bazhena96 <bazhenakak@gmail.com>
2020-09-09 00:09:56 +0300

    Check the the text on the page.

--- TestSuite/InvalidLoginScenarios.py
+++ TestSuite/InvalidLoginScenarios.py
@@ -1,8 +1,18 @@
+import gmail as gmail
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 import unittest
 
 from TestSuite.Utils import find_and_click, input_text, input_and_click_enter
+
+
+correctLoginIncorrectPassword = {"login": "bazhenkak@gmail.com", "password": 5645677, "error message": "Incorrect email/password – please check and retry"}
+incorrectLoginCorrectPassword = {"login": "gogogogo@gmail.com", "password": 7789938, "error message": "Incorrect email/password – please check and retry"}
+incorrectLoginIncorrectPassword = {'login': "goofojir@gmail.com", "password": 333333, "error message": "Incorrect email / password – please check and retry"}
+
+scenarios = [correctLoginIncorrectPassword, incorrectLoginCorrectPassword, incorrectLoginIncorrectPassword]
+
 
 class InvalidLogin(unittest.TestCase):
     def setUp(self):
@@ -13,9 +23,17 @@ class InvalidLogin(unittest.TestCase):
         self.driver.get("https://www.adidas.com")
 
     def test_login_scenario(self):
-
-
-
+        for i in scenarios:
+            find_and_click(self.driver, ".inner___1T3DW > a:nth-child(12)")
+            input_text(self.driver, "#login-email", "[login]")
+            input_text(self.driver, "#login-password", "[password]")
+            find_and_click(self.driver, "div.gl-vspace-bpall-small:nth-child(6) > button:nth-child(1)")
+            try:
+                text = self.driver.find_element_by_id("[error message")
+                return True
+            except NoSuchElementException:
+                print('Zero element')
+                return False
 
     def tearDown(self):
             self.driver.quit()
