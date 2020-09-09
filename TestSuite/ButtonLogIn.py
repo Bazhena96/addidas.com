c1f6df5a44ddf702f985caa569bd9622b1cc82c4
Bazhena96 <bazhenakak@gmail.com>
2020-09-09 22:01:25 +0300

    File added.

---
+++ TestSuite/ButtonLogIn.py
@@ -0,0 +1,26 @@
+import gmail as gmail
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+import unittest
+
+from TestSuite.Utils import find_and_click, input_text, input_and_click_enter
+
+
+class InvalidLogin(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)  # seconds
+        # navigate to the home page
+        self.driver.get("https://www.adidas.com")
+
+    def test_facebook_logIn(self):
+
+
+
+
+
+    def tearDown(self):
+            self.driver.quit()
+
