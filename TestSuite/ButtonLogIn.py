bd36914aa6cad6dde78cc0916af7cc72244d2267
Bazhena96 <bazhenakak@gmail.com>
2020-09-14 10:02:13 +0300

    Test to check facebook, google and yahoo added.

--- TestSuite/ButtonLogIn.py
+++ TestSuite/ButtonLogIn.py
@@ -16,11 +16,28 @@ class InvalidLogin(unittest.TestCase):
         self.driver.get("https://www.adidas.com")
 
     def test_facebook_logIn(self):
+        find_and_click(self.driver, ".inner___1T3DW > a:nth-child(12)")
+        facebook_button = self.driver.find_element_by_css_selector("button.gl-cta--secondary:nth-child(1)")
+        if facebook_button.is_enabled():
+            facebook_button.click()
+        else:
+            print("button not found")
 
+    def test_google_logIn(self):
+        find_and_click(self.driver, ".inner___1T3DW > a:nth-child(12)")
+        google_button = self.driver.find_element_by_css_selector("button.gl-cta:nth-child(2)")
+        if google_button.is_enabled():
+            google_button.click()
+        else:
+            print("Button not found")
 
-
-
+    def test_yahoo_logIn(self):
+        find_and_click(self.driver, ".inner___1T3DW > a:nth-child(12)")
+        yahoo_button = self.driver.find_element_by_css_selector("button.gl-cta:nth-child(3)")
+        if yahoo_button.is_enabled():
+            yahoo_button.click()
+        else:
+            print("Button not found")
 
     def tearDown(self):
-            self.driver.quit()
-
+        self.driver.quit()
